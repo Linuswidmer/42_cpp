bea7ee96a639a4ba296595d30ffedd720e1e98ca
Linus Widmer <lwidmer@4b0c17.42berlin.de>
2023-06-07 15:42:28 +0200

    adding timestamp and timestamp comparisons

--- ex00/2_megaphone.cpp
+++ ex00/2_megaphone.cpp
@@ -1,45 +1,60 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   2_megaphone.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lwidmer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/06 14:39:38 by lwidmer           #+#    #+#             */
-/*   Updated: 2023/06/07 11:21:15 by lwidmer          ###   ########.fr       */
+/*   Created: 2023/06/07 10:52:35 by lwidmer           #+#    #+#             */
+/*   Updated: 2023/06/07 11:11:37 by lwidmer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #ifndef MEGAPHONE_H
 # define MEGAPHONE_H
+
 # include <iostream>
-#endif
 
-std::string ConvertToUppercase(const std::string& str) 
-{
-    std::string result;
-    result.reserve(str.length());
+class Megaphone {
 
-    for (char c : str)
+private:
+	std::string UppercaseString;
+
+public:
+	Megaphone(const std::string& str)
 	{
-        result.push_back(std::toupper(c));
-    }
-    return result;
-}
+		UppercaseString = ConvertToUppercase(str);
+	}
+	std::string GetUppercaseString() const 
+	{
+		return (UppercaseString);
+	}
+private:
+	std::string ConvertToUppercase(const std::string& str) 
+	{
+    	std::string result;
+    	result.reserve(str.length());
+
+    	for (char c : str)
+    	{
+        	result.push_back(std::toupper(c));
+    	}
+    	return result;
+	}
+};
+#endif
 
 int main(int argc, char **argv)
 {
-	//Megaphone obj;
-
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
 	{
-		for (int i = 1; i < argc; ++i)
+		for (int i = 1;  i < argc; ++i)
 		{
-			std::string arg(argv[i]);
-			std::cout << ConvertToUppercase(arg);
+			Megaphone megaphone(argv[i]);
+			std::cout << megaphone.GetUppercaseString();
 		}
 		std::cout << std::endl;
 	}
-	return (0);
 }
