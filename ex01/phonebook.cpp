2d7d834d87e7be12f0cfa015376016202c92c72c
Linus Widmer <lwidmer@4b0c17.42berlin.de>
2023-08-30 12:38:44 +0200

    Phonebook fix

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -6,7 +6,7 @@
 /*   By: lwidmer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 11:22:08 by lwidmer           #+#    #+#             */
-/*   Updated: 2023/06/22 18:16:30 by lwidmer          ###   ########.fr       */
+/*   Updated: 2023/06/22 18:23:59 by lwidmer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,53 +14,8 @@
 # define PHONEBOOK_H
 # include <iostream>
 # include <string>
-# include <vector>
 # include <iomanip>
-# include <ctime>
-# include <chrono>
 
-class Contact 
-{
-public:
-	int			index;
-	//std::time_t	timestamp;
-	struct std::tm timestamp;
-	std::string FirstName;
-	std::string LastName;
-	std::string NickName;
-	std::string PhoneNumber;
-	std::string DarkestSecret;
-};
-
-std::tm	GetCurrentTimestamp(void)
-{
-	using std::chrono::system_clock;
-  	std::time_t tt = system_clock::to_time_t (system_clock::now());
-	std::tm localtime = *std::localtime(&tt);
-	return (localtime);
-}
-
-bool	CompareTimestamp(const Contact& contact1, const Contact& contact2)
-{
-	std::time_t time1 = std::mktime(const_cast<std::tm*>(&contact1.timestamp));
-	std::time_t time2 = std::mktime(const_cast<std::tm*>(&contact2.timestamp));
-	return (time1 < time2);
-}
-
-int		GetIndexOfLowestTimestamp(const std::vector<Contact> contacts)
-{
-	Contact LowestTimestamp;
-
-	LowestTimestamp = contacts[0];
-	for (int i = 1; i < contacts.size(); ++i)
-	{
-		if (CompareTimestamp(contacts[i], LowestTimestamp))
-		{
-			LowestTimestamp = contacts[i];
-		}
-	}
-	return (LowestTimestamp.index);
-}
 
 std::string	adjStringToTen(std::string str)
 {
@@ -70,73 +25,84 @@ std::string	adjStringToTen(std::string str)
 		return (str.append(10 - str.length(), ' '));
 }
 
+class Contact 
+{
+	public:
+
+	// Default constructor for the Contact class
+	Contact() :  FirstName(""), LastName(""), NickName(""),
+			PhoneNumber(""), DarkestSecret("") {
+    }
+	Contact(const std::string& FirstName, const std::string& LastName, 
+			const std::string& NickName, const std::string& PhoneNumber,
+			const std::string& DarkestSecret)
+			: FirstName(FirstName), LastName(LastName), NickName(NickName),
+			PhoneNumber(PhoneNumber), DarkestSecret(DarkestSecret) {
+    };
+
+	//private:
+		std::string FirstName;
+		std::string LastName;
+		std::string NickName;
+		std::string PhoneNumber;
+		std::string DarkestSecret;
+};
+
+// PhoneBook::~PhoneBook()
+// {
+//     std::cout << "PhoneBook destructor called!" << std::endl;
+// }
+
+
 class PhoneBook 
 {
-public:
-	std::vector<Contact> contacts;
-	
-	void Add(const std::string& str)
-	{
-		Contact NewContact;
+	// Default constructor
+	public:
+		PhoneBook(){
+			this->index = 0;
+		}
+		int 		index;
+		Contact		ContactArray[8];
+		std::string FirstName;
+		std::string LastName;
+		std::string NickName;
+		std::string PhoneNumber;
+		std::string DarkestSecret;	
 
+	void Add(void)
+	{
+		// use getline here
+		// no empty line allowed
 		std::cout << "Enter First Name: ";
-		std::cin >> NewContact.FirstName;
-		/*
+		std::cin >> FirstName;
 		std::cout << "Enter Last Name: ";
-		std::cin >> NewContact.LastName;
+		std::cin >> LastName;
 		std::cout << "Enter Nick Name: ";
-		std::cin >> NewContact.NickName;
+		std::cin >> NickName;
 		std::cout << "Enter Phone Number: ";
-		std::cin >> NewContact.PhoneNumber;
+		std::cin >> PhoneNumber;
 		std::cout << "Enter Darkest Secret: ";
-		std::cin >> NewContact.DarkestSecret;
+		std::cin >> DarkestSecret;
+		ContactArray[index % 8] = Contact(FirstName, LastName, NickName, PhoneNumber, DarkestSecret);
+		index++;
 		return ;
-		*/
-		AddContactToPhoneBook(NewContact);
 	};
-	void AddContactToPhoneBook(Contact NewContact)
-	{
-		int	SizeContacts;
-		int IndexOldestContact;
 
-		SizeContacts = contacts.size();
-		if (SizeContacts <= 2)
-		{
-			NewContact.index  = SizeContacts;
-			NewContact.timestamp = GetCurrentTimestamp();
-			contacts.push_back(NewContact);
-		}
-		else
-		{
-			IndexOldestContact = GetIndexOfLowestTimestamp(contacts);
-			NewContact.index = IndexOldestContact;
-			NewContact.timestamp = GetCurrentTimestamp();
-			ReplaceContactByIndex(NewContact);
-		}
-	};
-	void Search(const std::string& str)
+	void Search(void)
 	{
-		DisplayContacts();
+		DisplayAllContacts();
 	};
-	void DisplayContacts(void)
+
+	void DisplayAllContacts(void)
 	{
-		for (Contact c : contacts)
+		for (int i = 0; i < index && i < 8; i++)
 		{
-			std::cout	<< adjStringToTen(std::to_string(c.index))	<< "|"
-						<< adjStringToTen(c.FirstName) 				<< "|"
-						//<< adjStringToTen(c.LastName) 				<< "|"
-						//<< adjStringToTen(c.NickName) 				<< "|"
+			std::cout	<< i << "         "								<< "|"
+						<< adjStringToTen(ContactArray[i].FirstName) 	<< "|"
+						<< adjStringToTen(ContactArray[i].LastName) 	<< "|"
+						<< adjStringToTen(ContactArray[i].NickName) 	<< "|"
 						<< std::endl;
-						//<< std::put_time(const_cast<std::tm*>(&c.timestamp), "%Y-%m-%d %H:%M:%S") 
 		}
-	};
-	void ReplaceContactByIndex(const Contact NewContact)
-	{
-		int IndexOldestContact;
-
-		IndexOldestContact = NewContact.index;
-		contacts[IndexOldestContact] = NewContact;
-		return ;
 	};
 };
 
@@ -157,30 +123,23 @@ int	display_prompts(void)
 
 int main()
 {
-	int			exit;
-	std::string	input;	
-	PhoneBook 	phonebook;
+	int						exit;
+	std::string				input;	
+	PhoneBook 				phonebook;
 
 	exit = display_prompts();
 	while (1)
 	{
 		std::cin >> input;
-		if (std::cin.fail())
+		if (input == "ADD")
+			phonebook.Add();
+		else if (input == "SEARCH")
 		{
-			std::cerr << "error occured, exiting program" << std::endl;
-			return (1);
+			phonebook.Search();
+			// Prompt for one specific contact
 		}
-		else
-		{
-			if (input == "ADD")
-			{
-				phonebook.Add(input);
-			}
-			else if (input == "SEARCH")
-				phonebook.Search(input);
-			else if (input == "EXIT")
-				break ;
-		}
+		else if (input == "EXIT")
+			break ;
 	}
 	return (0);
 }
